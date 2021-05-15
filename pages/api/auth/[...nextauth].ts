00b9d6e6a0d92f6a16d00f2acddc822c40742de2
Masayuki Yamaji <afact_of_1ife@yahoo.co.jp>
2021-05-15 14:16:48 +0900

    認証の修正

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,42 @@
+import { NextApiHandler } from "next";
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+import Adapters from "next-auth/adapters";
+import prisma from "../../../lib/prisma";
+
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+export default authHandler;
+
+const options = {
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  adapter: Adapters.Prisma.Adapter({ prisma }),
+  secret: process.env.SECRET,
+  callbacks: {
+    signIn: async (profile, account, metadata) => {
+      const res = await fetch("https://api.github.com/user/emails", {
+        headers: {
+          Authorization: `token ${account.accessToken}`,
+        },
+      });
+      const emails = await res.json();
+      if (!emails || emails.length === 0) {
+        return;
+      }
+      const sortedEmails = emails.sort((a, b) => b.primary - a.primary);
+      profile.email = sortedEmails[0].email;
+      await prisma.user.update({
+        where: {
+          id: profile.id
+        },
+        data: {
+          ...profile
+        }
+      })
+    },
+  },
+};
